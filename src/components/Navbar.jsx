96ffb53bed7cf31333b902d35437df98f7716b3d
Javier Grau <javiergrau.freelance@gmail.com>
2023-07-18 08:30:35 -0400

    Code along with 'Redux Toolkit Tutorial'

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { CartIcon } from '../icons';
+import { useSelector } from 'react-redux';
+
+export const Navbar = () => {
+  const amount = useSelector((store) => store.cart.amount);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>redux toolkit</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
